9ecba3f6ef79cfcb2197c5fd540378c6eca6d3ff
Sammy Libre <sammy007@users.noreply.github.com>
2016-08-19 22:17:39 +0500

    Initial import

---
+++ js/custom.js
@@ -0,0 +1,22 @@
+// --------------------------------------------------------
+//	Navigation Bar
+// -------------------------------------------------------- 	
+$(window).scroll(function(){	
+	"use strict";	
+	var scroll = $(window).scrollTop();
+	if( scroll > 60 ){		
+		$(".navbar").addClass("scroll-fixed-navbar");				
+	} else {
+		$(".navbar").removeClass("scroll-fixed-navbar");
+	}
+});
+
+// --------------------------------------------------------
+//	Smooth Scrolling
+// -------------------------------------------------------- 	
+$(".navbar-nav li a[href^='#']").on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $(this.hash).offset().top
+    }, 1000);
+});
