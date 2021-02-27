a25896704592c5b4b2cc6fcfd73604d38d52ebee
tyuvvdgzkp <39473497+aeon1234@users.noreply.github.com>
2021-02-27 02:36:29 +0100

    multi-page layout

--- js/custom.js
+++ js/custom.js
@@ -1,35 +1,14 @@
-// --------------------------------------------------------
-//	Navigation Bar
-// -------------------------------------------------------- 	
-$(window).scroll(function(){	
-	"use strict";	
-	var scroll = $(window).scrollTop();
-	if( scroll > 60 ){		
-		$(".navbar").addClass("scroll-fixed-navbar");				
-	} else {
-		$(".navbar").removeClass("scroll-fixed-navbar");
-	}
-});
-
-// --------------------------------------------------------
-//	Smooth Scrolling
-// -------------------------------------------------------- 	
-$(".navbar-nav li a[href^='#']").on('click', function(e) {
-    e.preventDefault();
-    $('html, body').animate({
-        scrollTop: $(this.hash).offset().top
-    }, 1000);
-});
-
-// --------------------------------------------------------
-//	Language Dropdown
-// -------------------------------------------------------- 
 $(function() {
-	$('.langbtn').click(function(e){
-    	$('.lang-dropdown-child').addClass('show-child');
-    	e.stopPropagation();
-	})
-	$('body').click(function(){
-    	$('.lang-dropdown-child').removeClass('show-child');
-  	});
+  
+  // --------------------------------------------------------
+  //  Language Dropdown
+  // -------------------------------------------------------- 
+  $('.langbtn').click(function(e){
+    $('.lang-dropdown-child').addClass('show-child');
+      e.stopPropagation();
+  })
+  $('body').click(function(){
+    $('.lang-dropdown-child').removeClass('show-child');
+  });
+  
 });
